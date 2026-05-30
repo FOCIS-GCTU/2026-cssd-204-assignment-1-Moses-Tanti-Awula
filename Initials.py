87e4346dfb744f62b1e9d0c097bcd899c53b1f8d
Moses-Tanti-Awula <2425401995@live.gctu.edu.gh>
2026-05-30 22:29:35 +0000

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,39 @@
+# File: MTA.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Moses Tanti Awula
+# STUDENT ID: 2425401995
+# Email: 2425401995@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Moses Tanti Awula , this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+
+def main():
+    # Print my initials in small and large letters
+
+    print()
+
+    print("...MTA")
+
+    print()
+
+    print("...MMMM....MMMM...TTTTTTTTTTTT...AAAAAAAAAAAA.....")
+    print("...MMMM....MMMM...TTTTTTTTTTTT...AAAAAAAAAAAA.....")
+    print("...MM.MM..MM.MM.......TTTT.......AA........AA.....")
+    print("...MM.MM..MM.MM.......TTTT.......AA........AA.....")
+    print("...MM..MMMM..MM.......TTTT.......AAAAAAAAAAAA.....")
+    print("...MM..MMMM..MM.......TTTT.......AAAAAAAAAAAA.....")
+    print("...MM...MM...MM.......TTTT.......AA........AA.....")
+    print("...MM...MM...MM.......TTTT.......AA........AA.....")
+    print("...MM...MM...MM..**...TTTT..**...AA........AA...**")
+    print("...MM...MM...MM..**...TTTT..**...AA........AA...**")
+
+    print()
+
+
+
+main()
